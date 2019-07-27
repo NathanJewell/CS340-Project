c5447ce553a937f50214eac138e2733b4528cea1
Molly Johnson <johnsmol@flip1.engr.oregonstate.edu>
2019-07-27 08:29:26 -0700

    molly's completed ddl files, blank data manipulation files

--- sql/ddl/neighbor.sql
+++ sql/ddl/neighbor.sql
@@ -1,1 +1,13 @@
-molly
+-- Molly
+DROP TABLE IF EXISTS neighbor;
+
+CREATE TABLE neighbor(
+person_1_id INT DEFAULT NULL,
+person_2_id INT DEFAULT NULL,
+CONSTRAINT `person_1_id_fk`
+	FOREIGN KEY (person_1_id) REFERENCES person(id)
+	ON DELETE SET NULL,
+CONSTRAINT `person_2_id_fk`
+	FOREIGN KEY (person_2_id) REFERENCES person(id)
+	ON DELETE SET NULL
+) ENGINE = InnoDB;
--- sql/ddl/owner.sql
+++ sql/ddl/owner.sql
@@ -1,1 +1,13 @@
-molly
+-- Molly
+DROP TABLE IF EXISTS owner;
+
+CREATE TABLE owner(
+	person_id INT DEFAULT NULL,
+	house_id INT DEFAULT NULL,
+	CONSTRAINT `person_id_fk`
+		FOREIGN KEY (person_id) REFERENCES person (id)
+		ON DELETE SET NULL,
+	CONSTRAINT `house_id_fk`
+		FOREIGN KEY (house_id) REFERENCES house (id)
+		ON DELETE SET NULL
+) ENGINE = InnoDB;
