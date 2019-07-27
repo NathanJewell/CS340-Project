4be21f861b6cdeace491aa2a354bece9d5149a4d
Molly Johnson <johnsmol@oregonstate.edu>
2019-07-27 17:55:15 +0000

    for neighbor and owner: added pk, set all keys to not null, changed to on delete cascade

--- sql/ddl/neighbor.sql
+++ sql/ddl/neighbor.sql
@@ -2,12 +2,12 @@
 DROP TABLE IF EXISTS neighbor;
 
 CREATE TABLE neighbor(
-person_1_id INT DEFAULT NULL,
-person_2_id INT DEFAULT NULL,
-CONSTRAINT `person_1_id_fk`
-	FOREIGN KEY (person_1_id) REFERENCES person(id)
-	ON DELETE SET NULL,
-CONSTRAINT `person_2_id_fk`
-	FOREIGN KEY (person_2_id) REFERENCES person(id)
-	ON DELETE SET NULL
+	person_1_id INT PRIMARY KEY NOT NULL,
+	person_2_id INT NOT NULL,
+	CONSTRAINT `person_1_id_fk`
+		FOREIGN KEY (person_1_id) REFERENCES person(id)
+		ON DELETE CASCADE,
+	CONSTRAINT `person_2_id_fk`
+		FOREIGN KEY (person_2_id) REFERENCES person(id)
+		ON DELETE CASCADE
 ) ENGINE = InnoDB;
--- sql/ddl/owner.sql
+++ sql/ddl/owner.sql
@@ -2,12 +2,12 @@
 DROP TABLE IF EXISTS owner;
 
 CREATE TABLE owner(
-	person_id INT DEFAULT NULL,
-	house_id INT DEFAULT NULL,
+	person_id INT PRIMARY KEY NOT NULL,
+	house_id INT NOT NULL,
 	CONSTRAINT `person_id_fk`
 		FOREIGN KEY (person_id) REFERENCES person (id)
-		ON DELETE SET NULL,
+		ON DELETE CASCADE,
 	CONSTRAINT `house_id_fk`
 		FOREIGN KEY (house_id) REFERENCES house (id)
-		ON DELETE SET NULL
+		ON DELETE CASCADE
 ) ENGINE = InnoDB;
