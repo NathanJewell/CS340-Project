7db0909db7e4175233447e5731fbd874d4da6390
Molly Johnson <johnsmol@oregonstate.edu>
2019-07-28 19:27:33 +0000

    draft of houseSearch

--- sql/houseSearch.sql
+++ sql/houseSearch.sql
@@ -6,7 +6,7 @@ SELECT H.style, H.constructed, H.purchased, H.bedrooms, H.bathrooms, H.sqft,
 	H.levels, H.house_value 
 FROM house H
 INNER JOIN address
-ON house.id = 
-(SELECT A.id FROM address A WHERE number LIKE '%' + @number_input + '%' 
- AND streetName LIKE '%' + @streetName_input + '%' AND city LIKE '%' + 
- @city_input + '%' AND postalCode LIKE '%' + @postalCode_input + '%' LIMIT 1);
+	ON house.id = 
+		(SELECT A.id FROM address A WHERE number LIKE '%' + @number_input + '%' 
+ 		AND streetName LIKE '%' + @streetName_input + '%' AND city LIKE '%' + 
+ 		@city_input + '%' AND postalCode LIKE '%' + @postalCode_input + '%' LIMIT 1);
