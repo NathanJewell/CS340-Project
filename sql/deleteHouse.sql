8db3e386beace4a2de314f37ec447aef8d6f1528
Molly Johnson <johnsmol@oregonstate.edu>
2019-07-28 22:13:21 +0000

    added draft of deleteHouse

---
+++ sql/deleteHouse.sql
@@ -0,0 +1,10 @@
+-- Molly
+
+-- given address info, find address id using subquery, then
+-- use inner join to find the matching house, delete that house.
+DELETE * FROM house H
+INNER JOIN address
+	ON H.id =
+		(SELECT A.id FROM address A WHERE number LIKE '%' + @number_input + '%' 
+ 		AND streetName LIKE '%' + @streetName_input + '%' AND city LIKE '%' + 
+ 		@city_input + '%' AND postalCode LIKE '%' + @postalCode_input + '%' LIMIT 1); 
