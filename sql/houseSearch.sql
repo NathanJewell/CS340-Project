2c54ceebef29ff33d1a30957f63b5e0aed8d806f
Molly Johnson <johnsmol@oregonstate.edu>
2019-07-28 19:26:08 +0000

    added draft of houseSearch

---
+++ sql/houseSearch.sql
@@ -0,0 +1,12 @@
+-- Molly
+
+-- given address info, find address id using subquery, then use inner join
+-- to select the matching house information for that address id
+SELECT H.style, H.constructed, H.purchased, H.bedrooms, H.bathrooms, H.sqft,
+	H.levels, H.house_value 
+FROM house H
+INNER JOIN address
+ON house.id = 
+(SELECT A.id FROM address A WHERE number LIKE '%' + @number_input + '%' 
+ AND streetName LIKE '%' + @streetName_input + '%' AND city LIKE '%' + 
+ @city_input + '%' AND postalCode LIKE '%' + @postalCode_input + '%' LIMIT 1);
